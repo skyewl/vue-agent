39ecb31f2964aa4bb409f6af211dca6328da486f
skyewl <skye_wy@163.com>
2018-08-23 16:43:55 +0800

    localhost

--- config/index.js
+++ config/index.js
@@ -17,7 +17,7 @@ module.exports = {
       }
     },
     // Various Dev Server settings
-    host: '192.168.221.99', // can be overwritten by process.env.HOST
+    host: 'localhost', // can be overwritten by process.env.HOST
     port: 9910, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
